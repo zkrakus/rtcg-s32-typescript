b8fd39490a79ebb91b4e602b5517072a53d34935
zkrakus <zkrakus@gmail.com>
2025-07-31 11:56:49 -0400

    feat: Reviewed the typical ts features ... generics, type inference, etc.

--- basics.ts
+++ basics.ts
@@ -24,18 +24,50 @@ let hobbies: string[];
 
 hobbies = ['Sports', 'Cooking'];
 
-let person : {
+type Person = {
     name: string,
     age: number,
 };
 
+let person : Person
 person = {
     name: 'Max',
     age: 32
 }
 
+let people : Person[];
+
 // Type Inference
 
-let course = 'React  - The Complete Guide'
-
+// let course = 'React  - The Complete Guide'
 // course = 12341; error
+
+// Union Types 
+
+let course: string | number = 'React  - The Complete Guide'
+
+course = 12341;
+
+// Functions & types
+
+function add(a: number , b: number) {
+    return a + b;
+}
+
+function printOutput(value: any){
+    console.log(value);
+}
+
+// Generics
+
+function insertAtBeginning<T>(array: T[], value: T){
+    const newArray = [value,...array];
+    return newArray;
+}
+
+const demoArray = [1,2,3];
+const updatedArray = insertAtBeginning(demoArray, -1); // [-1, 1, 2, 3]
+
+const demoArrayString = ['1','2','3'];
+const updatedArrayString = insertAtBeginning(demoArrayString, '-1 0'); // [-1, 1, 2, 3]
+updatedArrayString[0].split('')
